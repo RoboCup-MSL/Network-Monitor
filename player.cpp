f59cc9a7f04cc2557611d4e2fd556b6e6aed1233
Cesar <cesarx4s@gmail.com>
2018-03-19 23:11:28 +0000

    Initialize player class

--- player.cpp
+++ player.cpp
@@ -2,5 +2,70 @@
 
 player::player()
 {
+    sta_name = QString("");
+    sta_first_time_seen = QDateTime();
+    sta_last_time_seen = QDateTime();
+    sta_power=0;
+    sta_packets=0;
+    sta_team = QString("");
+    sta_connected = false;
+    sta_bandwith = 0;
+}
+
+player::player(QString name, QString mac, QString team)
+{
+    sta_name = name;
+    sta_mac = mac;
+    sta_team = team;
+}
+
+player::player(QString mac)
+{
+    sta_mac = mac;
+}
+
+void player::update(QDateTime first_seen, QDateTime last_seen, int packets, int power)
+{
+    sta_first_time_seen = first_seen;
+    sta_last_time_seen = last_seen;
+    sta_packets = packets;
+    sta_power = power;
 
 }
+
+QString player::name()
+{
+    return sta_name;
+}
+
+QDateTime player::firstTimeSeen()
+{
+    return sta_first_time_seen;
+}
+
+QDateTime player::lastTimeSeen(){
+    return sta_last_time_seen;
+}
+
+QString player::mac(){
+    return sta_mac;
+}
+
+void player::connected()
+{
+    sta_connected = true;
+}
+
+void player::disconnected()
+{
+    sta_connected = false;
+}
+
+bool player::isConnected()
+{
+    return sta_connected;
+}
+
+
+
+
--- player.h
+++ player.h
@@ -1,11 +1,43 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <string>
+#include <QDateTime>
+#include <QString>
 
 
 class player
 {
+private:
+    QString sta_name;
+    QString sta_mac;
+    QDateTime sta_first_time_seen;
+    QDateTime sta_last_time_seen;
+    int sta_power;
+    int sta_packets;
+    QString sta_team;
+    bool sta_connected;
+    int sta_bandwith;
 public:
+    //Constructors
     player();
+    player(QString name, QString mac, QString team);
+    player(QString mac);
+    //End Constructors
+
+    //Acessors
+    //Setters
+    void disconnected();
+    void connected();
+    void update(QDateTime first_seen, QDateTime last_seen, int packets, int power);
+
+    //Getters
+    QString name();
+    QString mac();
+    QDateTime firstTimeSeen();
+    QDateTime lastTimeSeen();
+    bool isConnected();
+
+
 };
 
 #endif // PLAYER_H
