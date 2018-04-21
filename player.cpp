9d950892b48fd05d7cda9f019f27c1a48797bf6f
Cesar <cesarx4s@gmail.com>
2018-04-21 21:41:06 +0000

    Fix Packets/sec counter

--- player.cpp
+++ player.cpp
@@ -32,18 +32,20 @@ void player::update(QDateTime first_seen, QDateTime last_seen, int packets, int 
 {
     // Calculate packets per second rough aproximation
 
-    if((sta_last_time_seen.isValid() == true) && (last_seen.isValid() == true) && (last_seen > sta_last_time_seen))
+    if((sta_last_time_seen.isValid() == true) && (last_seen.isValid() == true))
     {
         int diff_packets =  packets - sta_packets;
         qint64 diff_time_msec = sta_last_time_seen.msecsTo(last_seen);
         if(diff_time_msec >=0)
         sta_pkts_sec = diff_packets*1000/diff_time_msec;
     }else
-        if((first_seen.isValid() == true) && (last_seen.isValid() == true) && (last_seen > first_seen))
+        if((first_seen.isValid() == true) && (last_seen.isValid() == true))
         {
            qint64 diff_time_msec = first_seen.msecsTo(last_seen);
            if(diff_time_msec >=0)
            sta_pkts_sec = packets*1000/diff_time_msec;
+        }else{
+            sta_pkts_sec = 0;
         }
 
     sta_first_time_seen = first_seen;
